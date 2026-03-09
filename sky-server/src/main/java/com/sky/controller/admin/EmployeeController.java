bc57d0b182e1e6022219cbcba98ab5936401b0dc
Chen3543 <chen354342060@gmail.com>
2026-03-09 16:09:23 +0800

    菜品分页查询 批量删除业务功能实现

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -11,6 +11,7 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/admin/employee")
+@Api(tags = "员工相关接口" )
 @Slf4j
 public class EmployeeController {
 
@@ -83,7 +85,7 @@ public class EmployeeController {
     @ApiOperation("新增员工")
     public Result save(@RequestBody EmployeeDTO employeeDTO){
         log.info("新增员工：{}",employeeDTO);
-        System.out.println("当前线程的id" + Thread.currentThread().getId());
+//        System.out.println("当前线程的id" + Thread.currentThread().getId());
         employeeService.save(employeeDTO);
         return Result.success();
     }
